9fdac95279a185af653acba53cef62d156f6782c
Ouzy Mariko <ousmane.mariko89@gmail.com>
2015-02-25 22:36:35 -0500

    new pages and data

--- client/js/app.js
+++ client/js/app.js
@@ -9,7 +9,7 @@ var data = {
 						game3: 'Sweden',
 						game1_loc: 'Winnipeg',
 						game2_loc: 'Winnipeg',
-						game3_loc: 'Edmonton'
+						game3_loc: 'Edmonton',
 					},
 					{	id: 'bra',
 						group: 'Group E',
@@ -244,15 +244,526 @@ var data = {
 						game3_loc: 'Vancouver'
 					}],
 
-  stadium: [{location: 'Edmonton'}, {location: 'Moncton'}, {location: 'Montreal'}, {location: 'Ottawa'}, {location: 'Vancouver'}, {location: 'Winnipeg'}]
-}
+  				stadium: [{location: 'Edmonton', id: 1}, {location: 'Moncton', id: 2}, {location: 'Montreal', id: 3}, {location: 'Ottawa', id: 4}, {location: 'Vancouver', id: 5}, {location: 'Winnipeg', id: 6}]
+};
+
+var match = {
+	schedule: [{	
+						match_id: 1,
+						group: 'Group A',
+						home: 'Canada',
+						away: 'China',
+						home_id: 'can',
+						away_id: 'chi',
+						location: 'Edmonton',
+						location_id: 1,
+						stadium: "Commonwealth Stadium",
+						time: '18:00', // ET time
+						date: "June 6",
+						score: ''
+					},
+					{	
+						match_id: 2,
+						group: 'Group A',
+						home: 'New Zealand',
+						away: 'Netherlands',
+						home_id: 'nzl',
+						away_id: 'ned',
+						location: 'Edmonton',
+						location_id: 1,
+						stadium: "Commonwealth Stadium",
+						time: '21:00', // ET time
+						date: "June 6",
+						score: ''
+					},
+					{	
+						match_id: 3,
+						group: 'Group B',
+						home: 'Norway',
+						away: 'Thailand',
+						home_id: 'nor',
+						away_id: 'tld',
+						location: 'Ottawa',
+						location_id: 4,
+						stadium: "Lansdowne Stadium",
+						time: '13:00', // ET time
+						date: "June 7",
+						score: ''
+					},
+					{	
+						match_id: 4,
+						group: 'Group B',
+						home: 'Germany',
+						away: 'Ivory Coast',
+						home_id: 'ger',
+						away_id: 'ivc',
+						location: 'Ottawa',
+						location_id: 4,
+						stadium: "Lansdowne Stadium",
+						time: '16:00', // ET time
+						date: "June 7",
+						score: ''
+					},
+					{	
+						match_id: 5,
+						group: 'Group D',
+						home: 'Sweden',
+						away: 'Nigeria',
+						home_id: 'swe',
+						away_id: 'nig',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '16:00', // ET time
+						date: "June 8",
+						score: ''
+					},
+					{	
+						match_id: 6,
+						group: 'Group C',
+						home: 'Cameroon',
+						away: 'Ecuador',
+						home_id: 'cam',
+						away_id: 'ecu',
+						location: 'Vancouver',
+						location_id: 5,
+						stadium: "BC Place Stadium",
+						time: '19:00', // ET time
+						date: "June 8",
+						score: ''
+					},
+					{	
+						match_id: 7,
+						group: 'Group D',
+						home: 'United States',
+						away: 'Australia',
+						home_id: 'usa',
+						away_id: 'aus',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '19:30', // ET time
+						date: "June 8",
+						score: ''
+					},
+					{	
+						match_id: 8,
+						group: 'Group C',
+						home: 'Japan',
+						away: 'Switzerland',
+						home_id: 'jap',
+						away_id: 'sui',
+						location: 'Vancouver',
+						location_id: 5,
+						stadium: "BC Place Stadium",
+						time: '20:00', // ET time
+						date: "June 8",
+						score: ''
+					},
+					{	
+						match_id: 9,
+						group: 'Group F',
+						home: 'France',
+						away: 'England',
+						home_id: 'fra',
+						away_id: 'eng',
+						location: 'Moncton',
+						location_id: 2,
+						stadium: "Moncton Stadium",
+						time: '13:00', // ET time
+						date: "June 9",
+						score: ''
+					},
+					{	
+						match_id: 10,
+						group: 'Group E',
+						home: 'Spain',
+						away: 'Costa Rica',
+						home_id: 'spa',
+						away_id: 'crc',
+						location: 'Montreal',
+						location_id: 3,
+						stadium: "Olympic Stadium",
+						time: '16:00', // ET time
+						date: "June 9",
+						score: ''
+					},
+					{	
+						match_id: 11,
+						group: 'Group F',
+						home: 'Colombia',
+						away: 'Mexico',
+						home_id: 'col',
+						away_id: 'mex',
+						location: 'Moncton',
+						location_id: 2,
+						stadium: "Moncton Stadium",
+						time: '16:00', // ET time
+						date: "June 9",
+						score: ''
+					},
+					{	
+						match_id: 12,
+						group: 'Group E',
+						home: 'Brazil',
+						away: 'South Korea',
+						home_id: 'bra',
+						away_id: 'skr',
+						location: 'Montreal',
+						location_id: 3,
+						stadium: "Olympic Stadium",
+						time: '19:00', // ET time
+						date: "June 9",
+						score: ''
+					},
+					{	
+						match_id: 13,
+						group: 'Group B',
+						home: 'Germany',
+						away: 'Norway',
+						home_id: 'ger',
+						away_id: 'nor',
+						location: 'Ottawa',
+						location_id: 4,
+						stadium: "Lansdowne Stadium",
+						time: '16:00', // ET time
+						date: "June 11",
+						score: ''
+					},
+					{	
+						match_id: 14,
+						group: 'Group A',
+						home: 'China',
+						away: 'Netherlands',
+						home_id: 'chi',
+						away_id: 'ned',
+						location: 'Edmonton',
+						location_id: 3,
+						stadium: "Commonwealth Stadium",
+						time: '18:00', // ET time
+						date: "June 11",
+						score: ''
+					},
+					{	
+						match_id: 15,
+						group: 'Group B',
+						home: 'Ivory Coast',
+						away: 'Thailand',
+						home_id: 'ivc',
+						away_id: 'tld',
+						location: 'Ottawa',
+						location_id: 4,
+						stadium: "Lansdowne Stadium",
+						time: '19:00', // ET time
+						date: "June 11",
+						score: ''
+					},
+					{	
+						match_id: 16,
+						group: 'Group A',
+						home: 'Canada',
+						away: 'New Zealand',
+						home_id: 'can',
+						away_id: 'nzl',
+						location: 'Edmonton',
+						location_id: 3,
+						stadium: "Commonwealth Stadium",
+						time: '21:00', // ET time
+						date: "June 11",
+						score: ''
+					},
+					{	
+						match_id: 17,
+						group: 'Group D',
+						home: 'Australia',
+						away: 'Nigeria',
+						home_id: 'aus',
+						away_id: 'nig',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '17:00', // ET time
+						date: "June 12",
+						score: ''
+					},
+					{	
+						match_id: 18,
+						group: 'Group C',
+						home: 'Switzerland',
+						away: 'Ecuador',
+						home_id: 'sui',
+						away_id: 'ecu',
+						location: 'Vancouver',
+						location_id: 5,
+						stadium: "BC Place Stadium",
+						time: '19:00', // ET time
+						date: "June 12",
+						score: ''
+					},
+					{	
+						match_id: 19,
+						group: 'Group D',
+						home: 'United States',
+						away: 'Sweden',
+						home_id: 'usa',
+						away_id: 'swe',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '20:00', // ET time
+						date: "June 12",
+						score: ''
+					},
+					{	
+						match_id: 20,
+						group: 'Group C',
+						home: 'Japan',
+						away: 'Cameroon',
+						home_id: 'jap',
+						away_id: 'cam',
+						location: 'Vancouver',
+						location_id: 5,
+						stadium: "BC Place Stadium",
+						time: '22:00', // ET time
+						date: "June 12",
+						score: ''
+					},
+					{	
+						match_id: 21,
+						group: 'Group F',
+						home: 'France',
+						away: 'Colombia',
+						home_id: 'fra',
+						away_id: 'col',
+						location: 'Moncton',
+						location_id: 2,
+						stadium: "Moncton Stadium",
+						time: '13:00', // ET time
+						date: "June 13",
+						score: ''
+					},
+					{	
+						match_id: 22,
+						group: 'Group E',
+						home: 'Brazil',
+						away: 'Spain',
+						home_id: 'bra',
+						away_id: 'spa',
+						location: 'Montreal',
+						location_id: 3,
+						stadium: "Olympic Stadium",
+						time: '16:00', // ET time
+						date: "June 13",
+						score: ''
+					},
+					{	
+						match_id: 23,
+						group: 'Group F',
+						home: 'England',
+						away: 'Mexico',
+						home_id: 'eng',
+						away_id: 'mex',
+						location: 'Moncton',
+						location_id: 2,
+						stadium: "Moncton Stadium",
+						time: '16:00', // ET time
+						date: "June 13",
+						score: ''
+					},
+					{	
+						match_id: 24,
+						group: 'Group E',
+						home: 'South Korea',
+						away: 'Costa Rica',
+						home_id: 'skr',
+						away_id: 'crc',
+						location: 'Montreal',
+						location_id: 3,
+						stadium: "Olympic Stadium",
+						time: '19:00', // ET time
+						date: "June 13",
+						score: ''
+					},
+					{	
+						match_id: 25,
+						group: 'Group B',
+						home: 'Thailand',
+						away: 'Germany',
+						home_id: 'tld',
+						away_id: 'ger',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '16:00', // ET time
+						date: "June 15",
+						score: ''
+					},
+					{	
+						match_id: 26,
+						group: 'Group B',
+						home: 'Ivory Coast',
+						away: 'Norway',
+						home_id: 'ivc',
+						away_id: 'nor',
+						location: 'Moncton',
+						location_id: 2,
+						stadium: "Moncton Stadium",
+						time: '16:00', // ET time
+						date: "June 15",
+						score: ''
+					},
+					{	
+						match_id: 27,
+						group: 'Group A',
+						home: 'Netherlands',
+						away: 'Canada',
+						home_id: 'ned',
+						away_id: 'can',
+						location: 'Montreal',
+						location_id: 3,
+						stadium: "Olympic Stadium",
+						time: '19:30', // ET time
+						date: "June 15",
+						score: ''
+					},
+					{	
+						match_id: 28,
+						group: 'Group A',
+						home: 'China',
+						away: 'New Zealand',
+						home_id: 'chi',
+						away_id: 'nzl',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '19:00', // ET time
+						date: "June 15",
+						score: ''
+					},
+					{	
+						match_id: 29,
+						group: 'Group C',
+						home: 'Ecuador',
+						away: 'Japan',
+						home_id: 'ecu',
+						away_id: 'jap',
+						location: 'Winnipeg',
+						location_id: 6,
+						stadium: "Winnipeg Stadium",
+						time: '17:00', // ET time
+						date: "June 16",
+						score: ''
+					},
+					{	
+						match_id: 30,
+						group: 'Group C',
+						home: 'Switzerland',
+						away: 'Cameroon',
+						home_id: 'sui',
+						away_id: 'cam',
+						location: 'Edmonton',
+						location_id: 1,
+						stadium: "Commonwealth Stadium",
+						time: '17:00', // ET time
+						date: "June 16",
+						score: ''
+					},
+					{	
+						match_id: 31,
+						group: 'Group D',
+						home: 'Nigeria',
+						away: 'United States',
+						home_id: 'nig',
+						away_id: 'usa',
+						location: 'Vancouver',
+						location_id: 5,
+						stadium: "BC Place Stadium",
+						time: '20:00', // ET time
+						date: "June 16",
+						score: ''
+					},
+					{	
+						match_id: 32,
+						group: 'Group D',
+						home: 'Australia',
+						away: 'Sweden',
+						home_id: 'aus',
+						away_id: 'swe',
+						location: 'Edmonton',
+						location_id: 1,
+						stadium: "Commonwealth Stadium",
+						time: '20:00', // ET time
+						date: "June 16",
+						score: ''
+					},
+					{	
+						match_id: 33,
+						group: 'Group F',
+						home: 'Mexico',
+						away: 'France',
+						home_id: 'mex',
+						away_id: 'fra',
+						location: 'Ottawa',
+						location_id: 4,
+						stadium: "Lansdowne Stadium",
+						time: '16:00', // ET time
+						date: "June 17",
+						score: ''
+					},
+					{	
+						match_id: 34,
+						group: 'Group F',
+						home: 'England',
+						away: 'Colombia',
+						home_id: 'eng',
+						away_id: 'col',
+						location: 'Montreal',
+						location_id: 3,
+						stadium: "Olympic Stadium",
+						time: '16:00', // ET time
+						date: "June 17",
+						score: ''
+					},
+					{	
+						match_id: 35,
+						group: 'Group E',
+						home: 'Costa Rica',
+						away: 'Brazil',
+						home_id: 'crc',
+						away_id: 'bra',
+						location: 'Moncton',
+						location_id: 2,
+						stadium: "Moncton Stadium",
+						time: '19:00', // ET time
+						date: "June 17",
+						score: ''
+					},
+					{	
+						match_id: 36,
+						group: 'Group E',
+						home: 'South Korea',
+						away: 'Spain',
+						home_id: 'skr',
+						away_id: 'spa',
+						location: 'Ottawa',
+						location_id: 4,
+						stadium: "Lansdowne Stadium",
+						time: '19:00', // ET time
+						date: "June 17",
+						score: ''
+					}]
+};
+
 
 Template.main.helpers({  
   //populate the list of teams
   
 });
 
+
+
 Template.teamlist.helpers(data); 
+Template.schedule.helpers(match);
 
 Router.configure({
 	layoutTemplate: 'main',
@@ -262,6 +773,9 @@ Router.route('home', {
 	path: '/'
 });
 
+Router.route('schedule', {
+	path: '/schedule'
+});
 
 Router.route('maps', {
 	path: '/maps'
