a9973d5d24d2fd0b3952b1bef83d4b211088cda2
Ouzy Mariko <ousmane.mariko89@gmail.com>
2015-04-22 11:54:10 -0400

    home animations

--- client/views/main/main_template.js
+++ client/views/main/main_template.js
@@ -1,6 +1,18 @@
-Template.maps.rendered = function () {
+Template.main.rendered = function () {
 	$('.menu_overlay').addClass('expanded').addClass('first_time');
   $('.menu_toggle .fa').addClass('rotate45');
+  $('.menu_overlay.expanded.first_time > ul').velocity(
+  	{ 
+			translateY:'10%'
+		})
+		.velocity(
+		{ 
+			translateY:'0',
+			opacity: 1 
+		},
+		{ 
+			display: "block" 
+		},  "ease-out"); 
 }
 
 Template.main.events({
