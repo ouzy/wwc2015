475626b2603af78b94c5005d5835470051e5b477
Ouzy Mariko <ousmane.mariko89@gmail.com>
2015-06-11 15:46:04 -0400

    scores for today

---
+++ client/views/groups/groups.js
@@ -0,0 +1,21 @@
+Template.groups.rendered = function () {
+	
+	if ($('.groups_table').is('#group_A')) {
+
+	}
+	else if ($('.groups_table').is('#group_B')) {
+
+	}
+	else if ($('.groups_table').is('#group_C')) {
+
+	}
+	else if ($('.groups_table').is('#group_D')) {
+
+	}
+	else if ($('.groups_table').is('#group_E')) {
+
+	}
+	else if ($('.groups_table').is('#group_F')) {
+
+	}
+}
