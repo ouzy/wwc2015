b4bfac7b2c1dae8951aec4a52ce706e1c9f7cb2f
Ouzy Mariko <ousmane.mariko89@gmail.com>
2015-06-22 15:02:36 -0400

    matches for group stages

--- client/views/schedule/schedule.js
+++ client/views/schedule/schedule.js
@@ -1,19 +1,59 @@
 Template.schedule.rendered = function () {
 	var match_number = 0;
-	for (x = 0; x < 36; x++) {
-		console.log(x);
+	for (x = 0; x < 54; x++) {
 		addScore(x);
 	}
+
 	
 	function addScore(n) {
 		var num = n +1;
-		if (matches[n]['status']=='future'){
-			$('.team.home#match' + num).append(" - ");
-			$('.team.away#match' + num).prepend(" - ");
+		if (n < 36) {
+			if (matches[n]['status']=='future'){
+				$('.team.home#match' + num).append(" - ");
+				$('.team.away#match' + num).prepend(" - ");
+			}
+			else {
+				$('.team.home#match' + num).append(" "+matches[n]['home_team']['goals']);
+				$('.team.away#match' + num).prepend(matches[n]['away_team']['goals'] + " ");
+			}
 		}
 		else {
-			$('.team.home#match' + num).append(" "+matches[n]['home_team']['goals']);
-			$('.team.away#match' + num).prepend(matches[n]['away_team']['goals'] + " ");
+			var fixture = $('.fixture#match' + num);
+			console.log("num: " + num);
+			console.log("n: " + n);
+			console.table(matches[n]);
+			//console.log(matches[num]['home_team']['country']);
+			//console.log(num+" "+matches[n]['home_team']['country']);
+			//console.log ("match: " + n + "match num: " + num);
+			
+					
+			
+			//
+			//fixture.find('.team.away').prepend('.fixture#match' + num);
+			if (matches[n]['status'] =='future'){
+				if (matches[n]['home_team']['code'] == "TBD"){
+					fixture.find('.team.home').append(matches[n]['home_team']['team_tbd']);	
+				}
+				else {
+					fixture.find('.team.home').append(matches[n]['home_team']['country']);
+				}
+				if (matches[n]['away_team']['code'] == "TBD"){
+					fixture.find('.team.away').append(matches[n]['away_team']['team_tbd']);	
+				}
+				else {
+					fixture.find('.team.away').append(matches[n]['away_team']['country']);
+				}
+				fixture.find('.team.home').append(" - ");
+				fixture.find('.team.away').prepend(" - ");
+			}
+			else {
+				fixture.find('.team.home').append(matches[n]['home_team']['country'] + " ");
+				fixture.find('.team.home').append(matches[n]['home_team']['goals']);
+				
+
+				fixture.find('.team.away').prepend(matches[n]['away_team']['goals'] + " ");
+				fixture.find('.team.away').append(matches[n]['away_team']['country']);
+			}
 		}
 	}
 }
--- client/views/today_matches/today.js
+++ client/views/today_matches/today.js
@@ -1,22 +1,55 @@
 Template.today.rendered = function () {
 	var match_number = 0;
-	for (x = 0; x < 36; x++) {
+	for (x = 0; x < 54; x++) {
 		addScore(x);
 	}
 	function addScore(n) {
 		var num = n +1;
-		if (matches[n]['status']=='future'){
-			$('.flag .home#match' + num +' .score h2').append("-");
-			$('.flag .away#match' + num +' .score h2').append("-");
-		}
-		else {
-			$('.flag .home#match' + num +' .score h2').append(matches[n]['home_team']['goals']);
-			$('.flag .away#match' + num +' .score h2').append(matches[n]['away_team']['goals']);
-			if (matches[n]['status']=='completed'){ 
-					$('.flag .match_status#match' + num +' h2').append('FT');
+		if (n < 36) {
+			if (matches[n]['status']=='future'){
+				$('.flag .home#match' + num +' .score h2').append("-");
+				$('.flag .away#match' + num +' .score h2').append("-");
 			}
 			else {
-				$('.flag .match_status#match' + num +' h2').append(matches[n]['status']);
+				$('.flag .home#match' + num +' .score h2').append(matches[n]['home_team']['goals']);
+				$('.flag .away#match' + num +' .score h2').append(matches[n]['away_team']['goals']);
+				if (matches[n]['status']=='completed'){ 
+						$('.flag .match_status#match' + num +' h2').append('FT');
+				}
+				else {
+					$('.flag .match_status#match' + num +' h2').append(matches[n]['status']);
+				}
+			}
+		}
+		else {
+			var fixture = $('.fixture#match' + num);
+			console.log("num: " + num);
+			console.log("n: " + n);
+			console.table(matches[n]);
+			
+			if (matches[n]['status'] =='future'){
+				if (matches[n]['home_team']['code'] == "TBD"){
+					fixture.find('.team.home').append(matches[n]['home_team']['team_tbd']);	
+				}
+				else {
+					fixture.find('.team.home').append(matches[n]['home_team']['country']);
+				}
+				if (matches[n]['away_team']['code'] == "TBD"){
+					fixture.find('.team.away').append(matches[n]['away_team']['team_tbd']);	
+				}
+				else {
+					fixture.find('.team.away').append(matches[n]['away_team']['country']);
+				}
+				fixture.find('.team.home').append(" - ");
+				fixture.find('.team.away').prepend(" - ");
+			}
+			else {
+				fixture.find('.team.home').append(matches[n]['home_team']['country'] + " ");
+				fixture.find('.team.home').append(matches[n]['home_team']['goals']);
+				
+
+				fixture.find('.team.away').prepend(matches[n]['away_team']['goals'] + " ");
+				fixture.find('.team.away').append(matches[n]['away_team']['country']);
 			}
 		}
 	}
